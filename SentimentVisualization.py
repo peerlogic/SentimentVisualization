08131e2f3eb5a33511ae9c1f728ca54066a39def
ferry <ferryxo@gmail.com>
2018-05-22 16:45:22 -0400

    x-axis label uses fontSize defined in json. define width and height through URL query parameters

--- SentimentVisualization.py
+++ SentimentVisualization.py
@@ -391,7 +391,7 @@ def visualize(id):
      else:
          data = json.loads(rows[0][0])
          w = len(data['h_labels']) * 200 if w == None else w
-         h = len(data['v_labels']) * 60 if h == None else h
+         h = len(data['v_labels']) * 80 if h == None else h
          return render_template('visualization.html', json_data = rows[0], width=w, height=h)
 
 if __name__ == '__main__':
